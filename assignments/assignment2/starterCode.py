4a19f4984fb4a60ec20f32e36d4ce25d587f87d5
Seth Akins <sethlinc@Seths-MacBook-Pro-2.local>
2024-09-12 16:13:44 -0600

    continued working on code

--- assignments/assignment2/starterCode.py
+++ assignments/assignment2/starterCode.py
@@ -14,6 +14,8 @@ T = np.zeros((n_states, n_actions))
 # setup what the policy is we want to run
 policy = np.zeros((n_states, n_actions))
 
+# We can just set an arbitrary algorithm parameter here because I can and Adrian can't stop me
+THETA = 0.0001
 def compute_matrices():
     env.reset()
     for s in range(n_states):
@@ -30,13 +32,11 @@ def bellman_v(**kwargs):
     gamma = kwargs.get("gamma", 1)
     initial_value = kwargs.get("initial_value", 0)
 
-    # We can just set an arbitrary algorithm parameter here because I can and Adrian can't stop me
-    THETA = 0.001
-    delta = 0.002
+    delta = THETA + 1
 
     # initialize value function approximation
     vk = [initial_value] * n_states
-    vk1 = [0] * n_states
+    vk1 = [initial_value] * n_states
 
     # initalize bellman error array
     bellman_errors = []
@@ -58,11 +58,10 @@ def bellman_v(**kwargs):
                     if dynamics_prob == 0:
                         continue
                     # Now we can finally just do our value update
-                    reward = R[state_prime, action]
-                    value += action_prob * dynamics_prob * (reward + (gamma * vk[state_prime]))
+                    reward = R[state, action]
+                    value += (action_prob * dynamics_prob * (reward + (gamma * vk[state_prime])))
             vk1[state] = value
             delta = max(delta, abs(vk[state] - vk1[state]))
-            print(delta)
         error = 0
         for vs, vs1 in zip(vk, vk1) :
             error += abs(vs - vs1)
@@ -73,18 +72,53 @@ def bellman_v(**kwargs):
     return {"values": vk1, "bellman_errors": bellman_errors}
 
 
-# def bellman_q(**kwargs):
-#     return
-#
+def bellman_q(**kwargs):
+    # Get the values we need from good ol kwargs
+    gamma = kwargs.get("gamma", 1)
+    initial_value = kwargs.get("initial_value", 0)
+
+    delta = THETA + 1
+
+    # initialize q function approximations
+    qk = np.full((n_states, n_actions), initial_value)
+    qk1 = np.zeros((n_states, n_actions))
+
+    bellman_errors = [[], [], [], [], []]
+
+    while delta > THETA:
+        delta = 0
+        value = 0
+        # Here we do state action pairs to start, so we immediately loop through all state action pairs
+        for state in range(n_states):
+            for action in range(n_actions):
+                value = 0
+                for state_prime in range(n_states):
+                    dynamics_prob = P[state, action, state_prime]
+                    if dynamics_prob == 0:
+                        continue
+                    reward = R[state, action]
+                    inner_sum = 0
+                    for action_prime in range(n_actions):
+                        action_prob = policy[state_prime, action_prime]
+                        if action_prob == 0:
+                            continue
+                        inner_sum += action_prob * qk[state_prime, action_prime]
+                    value += dynamics_prob * (reward + (gamma * inner_sum))
+                qk1[state, action] = value
+                delta = max(delta, abs(qk[state, action] - qk1[state, action]))
+    return
+
 def plot_graphs():
-    gammas = [0.01, 0.5, 0.99]
+    gammas = [0.01, 0.5, 0.99, 0.9]
     for init_value in [-10, 0, 10]:
         fig, axs = plt.subplots(2, len(gammas))
         fig.suptitle(f"$V_0$: {init_value}")
         for i, gamma in enumerate(gammas):
             results = bellman_v(gamma=gamma, initial_value=init_value)
-            axs[0][i].imshow(results.get("values"))
-            axs[1][i].plot(results.get("bellman_errors"))
+            values = np.array(results['values']).reshape(3, 3)
+            errors = results['bellman_errors']
+            axs[0][i].imshow(values)
+            axs[1][i].plot(range(len(errors)), errors)
             axs[0][i].set_title(f'$\gamma$ = {gamma}')
 
         # fig, axs = plt.subplots(n_actions + 1, len(gammas))
