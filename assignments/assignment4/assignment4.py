d6ecc7c1e05bc771ee0a8083c8cbd6e26a492488
Seth Akins <sethlinc@Seths-MacBook-Pro-2.local>
2024-09-18 14:43:32 -0600

    Strange plot bugs

--- assignments/assignment4/assignment4.py
+++ assignments/assignment4/assignment4.py
@@ -23,21 +23,25 @@ for s in range(n_states):
 
 P = P * (1.0 - T[..., None])  # next state probability for terminal transitions is 0
 
+
 def bellman_q(pi, gamma):
     I = np.eye(n_states * n_actions)
-    P_under_pi = (
-        P[..., None] * pi[None, None]
-    ).reshape(n_states * n_actions, n_states * n_actions)
+    P_under_pi = (P[..., None] * pi[None, None]).reshape(
+        n_states * n_actions, n_states * n_actions
+    )
     return (
-        R.ravel() * np.linalg.inv(I - gamma * P_under_pi)
-    ).sum(-1).reshape(n_states, n_actions)
+        (R.ravel() * np.linalg.inv(I - gamma * P_under_pi))
+        .sum(-1)
+        .reshape(n_states, n_actions)
+    )
+
 
 def episode(env, Q, eps, seed):
     data = dict()
     data["s"] = []
     data["a"] = []
     data["r"] = []
-    s, _ = env.reset(seed=seed)
+    s, _ = env.reset(seed=int(seed))
     done = False
     while not done:
         a = eps_greedy_action(Q, s, eps)
@@ -49,34 +53,77 @@ def episode(env, Q, eps, seed):
         s = s_next
     return data
 
+
 def eps_greedy_probs(Q, eps):
     max_actions = np.argmax(Q, axis=1)
     probabilities = np.full((n_states, n_actions), eps / n_actions, dtype=float)
     for state in range(n_states):
-        probabilities[state, max_actions[state]] += (1 - eps)
+        probabilities[state, max_actions[state]] += 1 - eps
     return probabilities
+
 
 def eps_greedy_action(Q, s, eps):
     probs = eps_greedy_probs(Q, eps)
     choices = list(range(n_actions))
     return np.random.choice(choices, p=probs[s])
 
-def monte_carlo(env, Q, gamma, eps_decay, max_steps, episodes_per_iteration, use_is):
+
+def monte_carlo(
+    env, Q, gamma, eps_decay, max_steps, episodes_per_iteration, seed, use_is
+):
     # Initialize Values we Need
-    pi = np.full((n_states, n_actions), 1.0 / n_actions)
+    eps = 1
+    total_steps = 0
+    last_update_step = 0
+    bellman_errors = []
+    C = np.zeros((n_states, n_actions))
 
-    # Need an empty list for each state-action pair
-    returns = []
+    # Get initial error
+    error = bellman_error(Q, eps, gamma)
+
+    while total_steps < max_steps:
+        # Generate Episodes
+        episodes = []
+        for i in range(episodes_per_iteration):
+            data = episode(env, Q, eps, seed)
+            episodes.append(data)
+            episode_steps = len(data["s"])
+            total_steps += episode_steps
+            # decay epsilon
+            eps = max(eps - eps_decay / max_steps * episode_steps, 0.01)
+
+        # Update our Q function
+        for item in episodes:
+            G = 0
+            W = 1
+            # for i in reversed(range(len(item["s"]))):
+            #     G = gamma * G + item["r"][i]
+            # for s, a, r in zip(reversed(item["s"]), reversed(item["a"]), reversed(item["r"])):
+            #     G = gamma * G + r
+            # Not sure about the t + 1
+            for i in reversed(range(len(item["s"]) - 1)):
+                a = item["a"][i]
+                s = item["s"][i]
+                G = gamma * G + item["r"][i + 1]
+                C[s, a] += W
+                Q[s, a] += (W / C[s, a]) * (G - Q[s, a])
+
+        # Add our previous bellman error
+        bellman_errors[last_update_step:total_steps] = [error] * (total_steps - last_update_step)
+        last_update_step = total_steps
+        error = bellman_error(Q, eps, gamma)
+
+    return Q, bellman_errors
+
+
+def bellman_error(Q, eps, gamma):
+    pi = eps_greedy_probs(Q, eps)
+    expected_q = bellman_q(pi, gamma)
+    error = 0
     for state in range(n_states):
-        returns.append([])
         for action in range(n_actions):
-            returns[s].append([])
-
-    # Generate Episodes
-    episode = []
-
-    ...
-    return Q, be
+            error += abs(Q[state, action] - expected_q[state, action])
+    return error
 
 
 def error_shade_plot(ax, data, stepsize, **kwargs):
@@ -86,7 +133,10 @@ def error_shade_plot(ax, data, stepsize, **kwargs):
     (line,) = ax.plot(x, y, **kwargs)
     error = np.nanstd(data, axis=0)
     error = 1.96 * error / np.sqrt(data.shape[0])
-    ax.fill_between(x, y - error, y + error, alpha=0.2, linewidth=0.0, color=line.get_color())
+    ax.fill_between(
+        x, y - error, y + error, alpha=0.2, linewidth=0.0, color=line.get_color()
+    )
+
 
 def compute_q():
     init_value = 0.0
@@ -96,14 +146,17 @@ def compute_q():
 
     episodes_per_iteration = [1, 10, 50]
     decays = [1, 2, 5]
-    seeds = np.arange(50)
+    # seeds = np.arange(50)
+    seeds = np.arange(10)
 
-    results = np.zeros((
-        len(episodes_per_iteration),
-        len(decays),
-        len(seeds),
-        max_steps,
-    ))
+    results = np.zeros(
+        (
+            len(episodes_per_iteration),
+            len(decays),
+            len(seeds),
+            max_steps,
+        )
+    )
 
     fig, axs = plt.subplots(1, 2)
     plt.ion()
@@ -112,7 +165,17 @@ def compute_q():
     use_is = False  # repeat with True
     for ax, reward_noise_std in zip(axs, [0.0, 3.0]):
         ax.set_prop_cycle(
-            color=["red", "green", "blue", "black", "orange", "cyan", "brown", "gray", "pink"]
+            color=[
+                "red",
+                "green",
+                "blue",
+                "black",
+                "orange",
+                "cyan",
+                "brown",
+                "gray",
+                "pink",
+            ]
         )
         ax.set_xlabel("Steps")
         ax.set_ylabel("Absolute Bellman Error")
@@ -126,8 +189,17 @@ def compute_q():
                 for seed in seeds:
                     np.random.seed(seed)
                     Q = np.zeros((n_states, n_actions)) + init_value
-                    Q, be = monte_carlo(env, Q, gamma, decay / max_steps, max_steps, episodes, use_is)
-                    results[j, k, seed] = be
+                    Q, be = monte_carlo(
+                        env,
+                        Q,
+                        gamma,
+                        decay / max_steps,
+                        max_steps,
+                        episodes,
+                        seed,
+                        use_is,
+                    )
+                    results[j, k, seed] = be[0:2000]
                 error_shade_plot(
                     ax,
                     results[j, k],
@@ -141,5 +213,7 @@ def compute_q():
     plt.ioff()
     plt.show()
 
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     compute_q()
